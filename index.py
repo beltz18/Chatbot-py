45dfd432a93283987db3bebfee96987ccdce80aa
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-10-25 00:26:40 -0400

    deleted comment and json import

--- index.py
+++ index.py
@@ -1,34 +1,3 @@
-# import json
-# from flask          import Flask, render_template
-# from flask_socketio import SocketIO, send
-# # from Data.main      import ChatBotAssistant
-
-# app = Flask(__name__)
-# app.config["SECRET_KEY"] = "secret"
-# socketio = SocketIO(app)
-
-# # assistant = ChatBotAssistant('Data/data-bot.json', model_name="test_model")
-# # assistant.train_model()
-# # assistant.save_model()
-
-# @app.route('/')
-# def index():
-#   return render_template('index.html')
-
-# @socketio.on("message")
-# def handle_message(value):
-#   print(value)
-
-# # @app.route('/get-data/<string:data>', methods=['POST'])
-# # def get_data(data):
-# #   message = json.loads(data)
-# #   dataBot = assistant.request(message)
-# #   return render_template('index.html')
-
-# if __name__ == '__main__':
-#   socketio.run(app)
-
-import json
 from flask          import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 from Data.main      import ChatBotAssistant
