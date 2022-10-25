0eb6300d6241ae30d33d73dff736e8832cb41a55
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-10-25 00:08:13 -0400

    finished for now

--- index.py
+++ index.py
@@ -1,35 +1,54 @@
+# import json
+# from flask          import Flask, render_template
+# from flask_socketio import SocketIO, send
+# # from Data.main      import ChatBotAssistant
+
+# app = Flask(__name__)
+# app.config["SECRET_KEY"] = "secret"
+# socketio = SocketIO(app)
+
+# # assistant = ChatBotAssistant('Data/data-bot.json', model_name="test_model")
+# # assistant.train_model()
+# # assistant.save_model()
+
+# @app.route('/')
+# def index():
+#   return render_template('index.html')
+
+# @socketio.on("message")
+# def handle_message(value):
+#   print(value)
+
+# # @app.route('/get-data/<string:data>', methods=['POST'])
+# # def get_data(data):
+# #   message = json.loads(data)
+# #   dataBot = assistant.request(message)
+# #   return render_template('index.html')
+
+# if __name__ == '__main__':
+#   socketio.run(app)
+
 import json
-import asyncio
-
 from flask          import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from Data.main      import ChatBotAssistant
-
-app = Flask(__name__)
-app.config['SECRET'] = "BossGymAdmin1234"
-socket = SocketIO(app, cors_allowed_origins="*")
 
 assistant = ChatBotAssistant('Data/data-bot.json', model_name="test_model")
 assistant.train_model()
 assistant.save_model()
 
-@socket.on("message")
-def handle_message(message):
-  print("Received message: "+message)
-  if message != "User connected":
-    send(message, broadcast=True)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+socket = SocketIO(app)
 
-@app.route('/')
+@app.route("/")
 def index():
-  return render_template('index.html')
+  return render_template("index.html")
 
-@app.route('/get-data/<string:data>', methods=['POST'])
-def get_data(data):
-  message = json.loads(data)
-  dataBot = assistant.request(message)
-  asyncio.sleep(10)
-  print(dataBot)
-  return render_template('index.html')
+@socket.on("message")
+def handle_message(data):
+  dataBot = assistant.request(data['value'])
+  emit("res", dataBot)
 
-if __name__ == '__main__':
-  asyncio.run(app.run(debug=True))
+if __name__ == "__main__":
+  socket.run(app)
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,15 @@
-function send_data() {
-  const data  = document.getElementById("data").value
-  const xhr = new XMLHttpRequest()
-  xhr.open('POST', "/get-data/"+JSON.stringify(data))
-  xhr.send()
-  console.log(xhr.responseText)
-}
+var Socket = io()
+
+Socket.on("connect", () => {
+  console.log("connected!")
+  
+  $("#send").on("click", () => {
+    let value = $("#data").val()
+    Socket.emit("message", {value})
+    $("#data").val("")
+  })
+  
+  Socket.on("res", (data) => {
+    $(".message").html(`<span><b>Chatbot responde:</b></span> ${data}`)
+  })
+})
